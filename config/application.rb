f1566017147974455d3c4c24dc7524bd0b6a2147
Ian Yang <me@iany.me>
2012-12-24 02:27:24 +0800

    Set test generator to rspec explicitly

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,10 @@ module NineteenWu
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.test_framework :rspec, :fixture => true
+    end
+
   end
 end
