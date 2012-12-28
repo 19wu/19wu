4a0e83aec292ec525b66a308ee4e759e6cba3dd2
Albert Yu <dev.yufan@gmail.com>
2012-12-28 10:42:44 +0800

    Add simple user fixture

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :user do
+    login 'Sample User'
+    email 'user@example.org'
+    password 'D8&m3n\Z'
+    password_confirmation 'D8&m3n\Z'
   end
 end
