5641980487074a7965965d1933c1aed5456cf268
Albert Yu <dev.yufan@gmail.com>
2012-12-29 04:29:42 +0800

    Refs #81 Improve integration test for user registration

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -7,37 +7,12 @@ feature 'user registration' do
 
   before(:each) { visit '/' }
 
-  scenario 'success' do
+  scenario 'successfully' do
     fill_in 'user_login', with: user.login
     fill_in 'user_email', with: user.email
     fill_in 'user_password', with: user.password
-    fill_in 'user_password_confirmation', with: user.password_confirmation
 
     click_button submit
     expect(page).to have_content(I18n.t('devise.registrations.signed_up'))
-  end
-
-  context 'failure' do
-    scenario 'with an invalid email address' do
-      fill_in 'user_login', with: user.login
-      fill_in 'user_email', with: 'useratexampledotcom'
-      fill_in 'user_password', with: user.password
-      fill_in 'user_password_confirmation', with: user.password_confirmation
-
-      click_button submit
-      expect(page).to have_content '因为 1 个错误导致'
-    end
-
-    scenario 'with a blank login name' do
-      fill_in 'user_login', with: ''
-      fill_in 'user_email', with: user.email
-      fill_in 'user_password', with: user.password
-      fill_in 'user_password_confirmation', with: user.password_confirmation
-
-      click_button submit
-
-      # expect(page).to have_content '因为 1 个错误导致'
-      expect(page).to have_content(I18n.t('devise.registrations.signed_up'))
-    end
   end
 end
