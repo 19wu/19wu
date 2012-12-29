d51de1de95ab8e3e82f6d18e22ab420d4ff6eee5
Ian Yang <me@iany.me>
2012-12-29 20:23:43 +0800

    do not rely on the hard coded attribute value in factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,16 +4,8 @@ FactoryGirl.define do
   sequence(:email) { |n| "#{sample_login}#{n}@19wu.org".downcase }
 
   factory :user do
-    login '19wu'
-    email '19wu@19wu.org'
-    password 'D8&m3n$Z'
-    #password_confirmation 'D8&m3n$Z'
-  end
-
-  factory :random_user, parent: :user do
     login
     email
     password ['DJX5nvyX', 'GG83Sr4{', '_pW.2P*8', 'MH^IN3B_'].sample
-    #password_confirmation { password }
   end
 end
