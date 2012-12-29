361e1d89391810d15c4966a194e5ca12e8e895ee
saberma <mahb45@gmail.com>
2012-12-29 13:30:40 +0800

    case insensitive unique. close #100

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :login, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
-  validates :login, :presence => true, :uniqueness => true, :format => { :with => /[a-zA-Z0-9_]+/ }
+  validates :login, presence: true, :uniqueness { case_sensitive: false }, format: { with: /[a-zA-Z0-9_]+/ }
 end
