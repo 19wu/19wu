3bc443db70756ded39977eeda8be20757f92a4d5
Ian Yang <me@iany.me>
2012-12-30 18:08:02 +0800

    setup action mailer using YAML file

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,7 +27,8 @@ NineteenWu::Application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
+  # This will be overrided by config/initializers/setup_mail.rb
+  # config.action_mailer.delivery_method = :test
 
   # Raise exception on mass assignment protection for Active Record models
   config.active_record.mass_assignment_sanitizer = :strict
