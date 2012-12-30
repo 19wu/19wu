009c02556411fb5f02e91caec7b9cb3c0182e277
Ian Yang <me@iany.me>
2012-12-30 18:08:02 +0800

    setup action mailer using YAML file

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,13 @@
+NineteenWu::Application.configure do
+  config.action_mailer.delivery_method = Settings.email.delivery_method.to_sym
+  delivery_settings_key = "#{Settings.email.delivery_method}_settings"
+  if delivery_settings = Settings.email[delivery_settings_key]
+    config.action_mailer.send "#{delivery_settings_key}=", delivery_settings.symbolize_keys
+  end
+
+  config.action_mailer.default_url_options = {
+    :host => Settings.email.host
+  }
+
+  ActionMailer::Base.default :from => Settings.email.from
+end
