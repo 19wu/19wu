f29e26a7832cd408633cc2d6ac4a2ceb29363547
Ian Yang <me@iany.me>
2013-01-05 03:23:20 +0800

    Test posting event start_time using compound_start_time_attributes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,12 @@
 class Event < ActiveRecord::Base
+  extend CompoundDatetime::HasCompoundDatetime
+
+  has_compound_datetime :start_time
+  has_compound_datetime :end_time
+
   attr_accessible :content, :location, :start_time, :end_time, :title
+  attr_accessible :compound_start_time_attributes
+  attr_accessible :compound_end_time_attributes
 
   validates :title, :start_time, :location, presence: true
 end
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -3,10 +3,47 @@ require 'spec_helper'
 describe EventsController do
 
   describe "GET 'new'" do
-    it "returns http success" do
+    it "builds a new event" do
       get 'new'
-      response.should be_success
+      assigns[:event].should be_a_kind_of(Event)
+      assigns[:event].should be_a_new_record
+    end
+
+    it "renders the new event form" do
+      get 'new'
+      response.should render_template('new')
     end
   end
 
+  describe "POST 'create'" do
+    let(:valid_attributes) { attributes_for(:event) }
+    context 'with valid attributes' do
+      it 'creates the event' do
+        expect {
+          post 'create', :event => valid_attributes
+        }.to change{Event.count}.by(1)
+      end
+    end
+
+    context 'post with compound_start_time_attributes' do
+      let(:compound_start_time_attributes) do
+        {
+          'date' => '2013-12-31',
+          'hour' => '12',
+          'meridian' => 'pm',
+          'min' => '10',
+          'sec' => '30'
+        }
+      end
+      let(:attributes) do
+        valid_attributes.except('start_time').
+          merge('compound_start_time_attributes' => compound_start_time_attributes)
+      end
+
+      it 'creates event with date 2013-12-31 12:10:30' do
+        post 'create', :event => attributes
+        assigns[:event].start_time.should == Time.zone.parse('2013-12-31 12:10:30')
+      end
+    end
+  end
 end
