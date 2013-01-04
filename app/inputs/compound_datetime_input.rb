8e2e65ee6461ec9e958d8eae1e4833e505cec642
Ian Yang <me@iany.me>
2013-01-05 06:00:45 +0800

    Adjust compound datetime input style

--- app/inputs/compound_datetime_input.rb
+++ app/inputs/compound_datetime_input.rb
@@ -6,15 +6,15 @@ class CompoundDatetimeInput < SimpleForm::Inputs::Base
   def input
     datetime = @builder.object.send(attribute_name) || CompoundDatetime.new
     @builder.fields_for(attribute_name, datetime) do |f|
-      template.content_tag(:div, :class => 'compound-datetime') do
+      template.content_tag(:div, :class => 'compound-datetime row-fluid') do
         result = ''
-        result += template.content_tag(:span, :class => 'input-append date') do
-          f.text_field(:date, :class => 'datepicker') +
+        result += template.content_tag(:span, :class => 'input-append date span6') do
+          f.text_field(:date, :class => 'span8 datepicker') +
             template.content_tag(:span, :class => 'add-on') do
             template.content_tag(:i, '', :class => 'icon-calendar datepicker-trigger')
           end
         end
-        result += template.content_tag(:span, :class => 'time') do
+        result += template.content_tag(:span, :class => 'time span6') do
           f.select(:hour, HOUR_COLLECTION, {:include_blank => true}, :class => 'time-hour') +
             template.content_tag(:span, ' : ', :class => 'muted') +
             f.select(:min, MIN_COLLECTION, {:include_blank => true}, :class => 'time-min') +
