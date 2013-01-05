5f18139be4fb3b31478367b8a704a17eaec44124
Ian Yang <me@iany.me>
2013-01-05 17:54:00 +0800

    DeviseHelper will hide the one in devise, delete it

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -81,4 +81,13 @@ describe ApplicationHelper do
       end
     end
   end
+
+  describe '#render_password_label_with_forget_link' do
+    let(:object) { build :user }
+    subject { helper.render_password_label_with_forget_link(object) }
+
+    it { should have_selector('small > a') }
+    it { should have_content(object.class.human_attribute_name(:password)) }
+    it { should have_content(t('links.forget_pass')) }
+  end
 end
