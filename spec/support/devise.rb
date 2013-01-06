fe3cd6568a3147edb7205ab5f418eba58684a93b
Ian Yang <me@iany.me>
2013-01-06 16:42:52 +0800

    Make devise test macros more flexible

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,7 +1,44 @@
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f unless f.end_with?('devise.rb') }
+module DeviseControllerMacros # http://git.io/WHvARA
+  extend ActiveSupport::Concern
+
+  def login_user(user = nil)
+    user ||= FactoryGirl.create(:user, :confirmed)
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in user
+  end
+
+  module ClassMethods
+    def login_user(&block)
+      before(:each) do
+        login_user block.try(:call)
+      end
+    end
+  end
+end
+
+module DeviseFeatureMacros
+  extend ActiveSupport::Concern
+
+  def sign_in(user = nil)
+    user ||= FactoryGirl.create(:user, :confirmed)
+
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_button I18n.t('labels.sign_in')
+  end
+
+  module ClassMethods
+    def sign_in(&block)
+      before(:each) do
+        login_user block.try(:call)
+      end
+    end
+  end
+end
 
 RSpec.configure do |config| # http://git.io/WHvARA
   config.include Devise::TestHelpers, :type => :controller
-  config.include FeatureMacros, :type => :feature
-  config.extend ControllerMacros, :type => :controller
+  config.include DeviseFeatureMacros, :type => :feature
+  config.include DeviseControllerMacros, :type => :controller
 end
