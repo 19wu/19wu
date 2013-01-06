26a118dd38a3889b929eff9ed600767b363c692b
Ian Yang <me@iany.me>
2013-01-06 17:35:36 +0800

    Add profiles controller specs and implement
    Write detailed specs from feature

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,22 @@
+class ProfilesController < ApplicationController
+  prepend_before_filter :authenticate_user!
+  before_filter :find_profile
+
+  def show
+  end
+
+  def update
+    @profile.assign_attributes(params[:profile])
+
+    if @profile.save
+      redirect_to profile_path, :notice => I18n.t('flash.profiles.updated')
+    else
+      render :show
+    end
+  end
+
+  private
+  def find_profile
+    @profile = current_user.profile
+  end
+end
