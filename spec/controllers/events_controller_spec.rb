05633965518754e0308e17552f0c808b9d62c0b9
ywjno <ywjno.dev@gmail.com>
2013-01-08 22:56:31 +0800

    add test code by missions newcomer 1

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -64,4 +64,71 @@ describe EventsController do
       end
     end
   end
+
+  describe "PUT 'update'" do
+    let(:event) { FactoryGirl.create(:event) }
+    let(:valid_attributes) { attributes_for(:event) }
+    context 'when user has signed in' do
+      login_user
+      before do
+        subject.current_user.events.should_receive(:find).with(event.id.to_s).and_return(event)
+      end
+      context 'with valid attributes' do
+        it 'update the event' do
+          put 'update', :id => event.id, :event => valid_attributes
+          response.should redirect_to(event_path)
+        end
+      end
+
+      context 'update all attributes' do
+        let(:compound_start_time_attributes) do
+          {
+            'date' => '2013-01-08',
+            'hour' => '4',
+            'meridian' => 'pm',
+            'min' => '10',
+            'sec' => '30'
+          }
+        end
+        let(:compound_end_time_attributes) do
+          {
+            'date' => '2013-01-08',
+            'hour' => '6',
+            'meridian' => 'pm',
+            'min' => '10',
+            'sec' => '30'
+          }
+        end
+        let(:attributes) do
+          valid_attributes.except('start_time').
+            merge(
+                'title' => "19wu development meeting by issuse #185",
+                'location' => "Dalian, China",
+                'content' => "Dalian 19wu development meeting by issuse #185",
+                'location_guide' => "Best by Plant come here",
+                'compound_start_time_attributes' => compound_start_time_attributes,
+                'compound_end_time_attributes' => compound_end_time_attributes
+            )
+        end
+
+        it 'when all input changed' do
+          put 'update', :id => event.id, :event => attributes
+          assigns[:event].title.should == "19wu development meeting by issuse #185"
+          assigns[:event].location.should ==  "Dalian, China"
+          assigns[:event].content.should == "Dalian 19wu development meeting by issuse #185"
+          assigns[:event].location_guide.should == "Best by Plant come here"
+          assigns[:event].start_time.should == Time.zone.parse('2013-01-08 16:10:30')
+          assigns[:event].end_time.should == Time.zone.parse('2013-01-08 18:10:30')
+        end
+      end
+    end
+
+    context 'when user has not yet signed in' do
+      it "should be redirect" do
+        put 'update', :id => event.id, :event => valid_attributes
+        response.should be_redirect
+      end
+    end
+  end
+
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe HomeController do
 
   describe "GET 'index'" do
+    login_user
     it "returns http success" do
       get 'index'
       response.should be_success
     end
   end
-
 end
