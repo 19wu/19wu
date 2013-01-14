14371b630e8bcea1db4252eb18c7597c35f3231e
Meck <yesmeck@gmail.com>
2013-01-14 23:47:35 +0800

    Remove unnecessary class

--- app/inputs/uploadable_input.rb
+++ app/inputs/uploadable_input.rb
@@ -6,7 +6,7 @@ class UploadableInput < SimpleForm::Inputs::TextInput
       result += super
       result += template.content_tag(:p, :class => 'upload-panel') do
         template.content_tag(:span, :class => 'default') do
-          template.tag(:input, :type => 'file', :multiple => :multiple, 'data-url' => '/photos', :class => 'fileupload manual-file-chooser js-manual-file-chooser') +
+          template.tag(:input, :type => 'file', :multiple => :multiple, 'data-url' => '/photos', :class => 'fileupload manual-file-chooser') +
           template.content_tag(:a, I18n.t('simple_form.hints.photo.upload'), :href => '#')
         end +
         template.content_tag(:span, I18n.t('simple_form.hints.photo.uploading'), :class => 'uploading')
