7b67fcca1edee154c03c58865fbd8aea3b3eec7e
tony612 <h.bing612@gmail.com>
2013-01-21 13:15:41 +0800

    delete the users_helper file and the spec test

--- app/helpers/users_helper.rb
+++
@@ -1,9 +0,0 @@
-module UsersHelper
-  def gravatar_for(user, size = 36)
-    return '' if user.blank?
-
-    md_hash = Digest::MD5::hexdigest(user.email || '')
-    gravatar_url = "http://www.gravatar.com/avatar/#{md_hash}?s=#{size}&d=mm"
-    image_tag(gravatar_url, alt: user.login, class: 'gravatar')
-  end
-end
