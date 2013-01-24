0b8ac3ebb4554c6f659a4a5e5e32b9a327178a67
saberma <mahb45@gmail.com>
2013-01-24 10:20:39 +0800

    fixed rspec error.

--- spec/views/events/show.html.slim_spec.rb
+++ spec/views/events/show.html.slim_spec.rb
@@ -1,12 +1,13 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
-describe 'events/_event.html.slim' do
+describe 'events/show.html.slim' do
   let(:event) { build_stubbed(:event, :markdown) }
   let(:current_user) { build(:user) }
-
-  before { render :partial => 'events/event', :locals => { :event => event, :current_user => current_user } }
+  before do
+    assign :event, event
+    render :template => 'events/show', :locals => { :current_user => current_user}
+  end
   subject { rendered }
-
   it { should have_content(event.title) }
   it { should include(event.content_html) }
   it { should include(event.location_guide_html) }
