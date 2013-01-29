e30113aeb83e9f9ac8ed995c53c8fdb4e3bda902
saberma <mahb45@gmail.com>
2013-01-29 16:31:11 +0800

    use zeus to speed up rails. #227

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
+ENV["RAILS_ENV"] = 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
+#require 'rspec/autorun' # http://j.mp/WLeAs3
 require 'capybara/email/rspec'
 
 class Capybara::Email::Driver
