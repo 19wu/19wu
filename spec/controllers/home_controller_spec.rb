fb81a776e1ee0a9f43da473da4c993163f0918a0
PengJie Wu <cloudduke@gmail.com>
2013-02-03 19:59:38 +1100

    #229: add unit tests and functional test and improve codes

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -9,4 +9,13 @@ describe HomeController do
       response.should be_success
     end
   end
+
+  describe "POST content_preview" do
+    it "returns http response" do
+      post 'content_preview', :content => "# test"
+      response.should be_success
+      JSON.parse(response.body)["result"].should == "<h1>test</h1>"
+    end
+  end
+
 end
