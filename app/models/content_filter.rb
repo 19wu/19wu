48e923e6d9d7709dc70462089b017071a3de3c60
PengJie Wu <cloudduke@gmail.com>
2013-02-09 02:13:04 +1100

    #229: refactor the markdown filter method by using registered filters

--- app/models/content_filter.rb
+++ app/models/content_filter.rb
@@ -3,10 +3,8 @@ require 'html/pipeline'
 class ContentFilter
   
   def self.refine(content)
-    pipeline = HTML::Pipeline.new [
-      HTML::Pipeline::MarkdownFilter
-    ]
-    pipeline.call(content)[:output].to_s
+    filters = HasHtmlPipeline.registered_html_pipelines[:markdown]
+    filters.call(content)[:output].to_s
   end
 
 end
