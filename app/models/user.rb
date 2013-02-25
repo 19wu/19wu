55f243b12dd08617440ad15e308812501cbbf6bd
saberma <mahb45@gmail.com>
2013-02-25 17:19:57 +0800

    add devise invitable table for user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,:confirmable,
+  devise :invitable, :database_authenticatable, :registerable,:confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
