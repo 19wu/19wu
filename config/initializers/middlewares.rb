dae9a11f04941ceffae800739e1688a8a2abeda9
saberma <mahb45@gmail.com>
2013-05-16 23:07:43 +0800

    config exception recipients.

--- config/initializers/middlewares.rb
+++ config/initializers/middlewares.rb
@@ -4,5 +4,5 @@ if Rails.env.production?
   NineteenWu::Application.config.middleware.use ExceptionNotifier,
     :email_prefix => "[19wu] ",
     :sender_address => Settings.email.from,
-    :exception_recipients => %W{#{Settings.email.from}}
+    :exception_recipients => Settings.email.exception_recipients
 end
