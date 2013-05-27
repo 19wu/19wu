8e7cc7ad695540f71a339b01cc01b75023d5f95e
Michael Wang <ylzcylx@gmail.com>
2013-05-27 13:47:02 +0800

    #349 show event summary on event show page

--- app/models/event_summary.rb
+++ app/models/event_summary.rb
@@ -1,7 +1,12 @@
 class EventSummary < ActiveRecord::Base
+  extend HasHtmlPipeline
+
   attr_accessible :content, :event_id
+
   belongs_to :event
 
+  has_html_pipeline :content, :markdown
+  
   validates_presence_of :content
   validates :content, :length => { :minimum => 10 }
 end
