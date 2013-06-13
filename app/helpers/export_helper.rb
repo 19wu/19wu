ceadc74acc1ed9030fa59f2ae7e85c8320292817
saberma <mahb45@gmail.com>
2013-06-13 22:03:06 +0800

    fixed clippy missing text clippy method move to partial.

--- app/helpers/export_helper.rb
+++ app/helpers/export_helper.rb
@@ -12,34 +12,6 @@ module ExportHelper
     markdown.join("\n"*3)
   end
 
-  def clippy(text, bgcolor='#FFFFFF')
-    html = <<-EOF
-      <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
-              width="110"
-              height="14"
-              id="clippy" >
-      <param name="movie" value="/flash/clippy.swf"/>
-      <param name="allowScriptAccess" value="always" />
-      <param name="quality" value="high" />
-      <param name="scale" value="noscale" />
-      <param name="FlashVars" value='text=#{text}'>
-      <param name="bgcolor" value="#{bgcolor}">
-      <embed src="/flash/clippy.swf"
-             width="110"
-             height="14"
-             name="clippy"
-             quality="high"
-             allowScriptAccess="always"
-             type="application/x-shockwave-flash"
-             pluginspage="http://www.macromedia.com/go/getflashplayer"
-             FlashVars='text=#{text}'
-             bgcolor="#{bgcolor}"
-      />
-      </object>
-    EOF
-    html.html_safe
-  end
-
   private
   %w(title location content).each do |field|
     class_eval %Q{
