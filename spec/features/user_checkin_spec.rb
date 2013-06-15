c46fb7a1f9aca27b76a08d2a1eb039ea36e490f7
saberma <mahb45@gmail.com>
2013-06-15 22:15:26 +0800

    refactory check in feature test.

--- spec/features/user_checkin_spec.rb
+++ spec/features/user_checkin_spec.rb
@@ -1,63 +1,51 @@
 require 'spec_helper'
 
 feature "user check in" do
-  background do
-    @lilei = create(:user, :confirmed)
-    @hanmeimei = create(:user, :confirmed)
+  given(:lilei) { create(:user, :confirmed) }
+  given(:hanmeimei) { create(:user, :confirmed) }
+  given(:event) { create(:event, user: lilei, start_time: 0.day.since) }
+  given(:participant) { create(:event_participant, event: event, user: hanmeimei) }
+
+  context 'event is start today' do
+    context 'user has joined' do
+      before do
+        participant
+        login_user hanmeimei
+      end
+      scenario "checkin with valid checkin code", js: true do
+        visit event_path(event)
+        fill_in 'appendedInputButton', with: event.checkin_code
+        click_on I18n.t('labels.check_in_button')
+        expect(page).to have_content I18n.t('flash.participants.checkin_welcome')
+      end
+      scenario "checkin with invalid checkin code", js: true do
+        visit event_path(event)
+        fill_in 'appendedInputButton', with: event.checkin_code + "invalid"
+        click_on I18n.t('labels.check_in_button')
+        expect(page).to have_content I18n.t('flash.participants.checkin_wrong_checkin_code')
+      end
+      scenario "checkin twice" do
+        visit checkin_event_path(event, checkin_code: event.checkin_code)
+        visit checkin_event_path(event, checkin_code: event.checkin_code)
+        expect(page).to have_content I18n.t('flash.participants.checkin_more_than_1_time')
+      end
+    end
+    context 'user has not joined' do
+      scenario "checkin" do
+        login_user hanmeimei
+        visit checkin_event_path(event, checkin_code: event.checkin_code)
+        expect(page).to have_content I18n.t('flash.participants.checkin_need_join_first')
+      end
+    end
   end
 
-  scenario "checkin with valid checkin code", js: true do
-    @event = create(:event, user: @lilei, start_time: 0.day.since)
-    @participant = create(:event_participant, event: @event, user: @hanmeimei)
-    login_user @hanmeimei
-
-    visit event_path(@event)
-    expect(page).to have_content I18n.t('labels.check_in_button')
-    
-    fill_in 'appendedInputButton', with: @event.checkin_code
-    click_on I18n.t('labels.check_in_button')
-
-    expect(page).to have_content I18n.t('flash.participants.checkin_welcome')
-  end
-
-  scenario "checkin with invalid checkin code", js: true do
-    @event = create(:event, user: @lilei, start_time: 0.day.since)
-    @participant = create(:event_participant, event: @event, user: @hanmeimei)
-    login_user @hanmeimei
-
-    visit event_path(@event)
-    expect(page).to have_content I18n.t('labels.check_in_button')
-    
-    fill_in 'appendedInputButton', with: @event.checkin_code + "invalid"
-    click_on I18n.t('labels.check_in_button')
-
-    expect(page).to have_content I18n.t('flash.participants.checkin_wrong_checkin_code')
-  end
-
-  scenario "checkin 1 day before event start" do
-    @event = create(:event, user: @lilei, start_time: 1.day.since)
-    @participant = create(:event_participant, event: @event, user: @hanmeimei)
-    login_user @hanmeimei
-
-    visit checkin_event_path(@event, checkin_code: @event.checkin_code)
-    expect(page).to have_content I18n.t('flash.participants.checkin_in_need_the_same_day_of_event_starttime')
-  end
-
-  scenario "checkin without join event" do
-    @event = create(:event, user: @lilei, start_time: 0.day.since)
-    login_user @hanmeimei
-
-    visit checkin_event_path(@event, checkin_code: @event.checkin_code)
-    expect(page).to have_content I18n.t('flash.participants.checkin_need_join_first')
-  end
-
-  scenario "checkin twice" do
-    @event = create(:event, user: @lilei, start_time: 0.day.since)
-    @participant = create(:event_participant, event: @event, user: @hanmeimei)
-    login_user @hanmeimei
-
-    visit checkin_event_path(@event, checkin_code: @event.checkin_code)
-    visit checkin_event_path(@event, checkin_code: @event.checkin_code)
-    expect(page).to have_content I18n.t('flash.participants.checkin_more_than_1_time')
+  context 'event start tomorrow' do
+    given(:event) { create(:event, user: lilei, start_time: 1.day.since) }
+    scenario "checkin" do
+      participant
+      login_user hanmeimei
+      visit checkin_event_path(event, checkin_code: event.checkin_code)
+      expect(page).to have_content I18n.t('flash.participants.checkin_in_need_the_same_day_of_event_starttime')
+    end
   end
 end
