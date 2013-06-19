6ab2f93318c93792ae76627c2e6cb0fd2366c0a2
saberma <mahb45@gmail.com>
2013-06-19 22:22:29 +0800

    fixed rspec error.

--- spec/factories/event_changes.rb
+++ spec/factories/event_changes.rb
@@ -1,5 +1,4 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
+# encoding: utf-8
 FactoryGirl.define do
   factory :event_change do
     content "临时通知：深圳 Rubist 活动举办的时间由原来的6月15日（周六）改为6月16日（周日），请见谅。"
