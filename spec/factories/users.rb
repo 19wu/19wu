20beaa1154b05d4fb399470e53d0ea8cd7c641b1
saberma <mahb45@gmail.com>
2013-07-02 23:08:39 +0800

    event change send sms test.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,10 +2,12 @@ FactoryGirl.define do
   sample_login = ['jack', 'lucy', 'dave', 'lily', 'john', 'beth'].sample
   sequence(:login) { |n| "#{sample_login}#{n}" }
   sequence(:email) { |n| "#{sample_login}#{n}@19wu.org".downcase }
+  sequence(:phone) { |n| "1392845288#{n}"}
 
   factory :user do
     login
     email
+    phone
     password ['DJX5nvyX', 'GG83Sr4{', '_pW.2P*8', 'MH^IN3B_'].sample
 
     trait :confirmed do
--- spec/models/event_change_spec.rb
+++ spec/models/event_change_spec.rb
@@ -2,19 +2,29 @@
 require 'spec_helper'
 
 describe EventChange do
-  describe 'send email to participants' do
+  describe 'send change to participants' do
     let(:user) { create(:user, :confirmed) }
     let(:event) { create(:event, user: user, title: '深圳Rubyist活动') }
     let(:participant1) { create(:event_participant, :random_user, event: event) }
     let(:participant2) { create(:event_participant, :random_user, event: event) }
     let(:change) { create(:event_change, event: event) }
-    subject { ActionMailer::Base.deliveries.last }
     before do
       [participant1, participant2]
-      ActionMailer::Base.deliveries.clear
-      change
     end
-    its(:subject) { should eql '「重要」深圳Rubyist活动 变更通知' }
-    its('body.decoded') { should match change.content }
+    describe 'by email' do
+      subject { ActionMailer::Base.deliveries.last }
+      before do
+        ActionMailer::Base.deliveries.clear
+        change
+      end
+      its(:subject) { should eql '「重要」深圳Rubyist活动 变更通知' }
+      its('body.decoded') { should match change.content }
+    end
+    describe 'by sms' do
+      it 'should be success' do
+        ChinaSMS.stub(:to).with([participant1.user.phone, participant2.user.phone], I18n.t('sms.event.change', content: attributes_for(:event_change)[:content]))
+        change
+      end
+    end
   end
 end
