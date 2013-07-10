5e3bf1a8413567a3f21ce1665aecfd69d2039c1d
saberma <mahb45@gmail.com>
2013-07-10 22:58:44 +0800

    user phone code can send 10 times a day.

--- app/controllers/user_phones_controller.rb
+++ app/controllers/user_phones_controller.rb
@@ -18,11 +18,17 @@ class UserPhonesController < ApplicationController
   end
 
   def send_code
-    code = cookies.signed[:code] || rand(1000..9999)
-    code = 1234 if Rails.env.test?
-    Rails.logger.info I18n.t('sms.user.phone_code', code: code)
-    ChinaSMS.to params[:phone], I18n.t('sms.user.phone_code', code: code)
-    cookies.signed[:code] = { value: code, expires: 1.hour.from_now }
+    ip_key = "sms:#{request.remote_ip}"
+    send_times = Rails.cache.fetch(ip_key, expires_in: 24.hours){ 10 }
+    Rails.logger.info send_times
+    if send_times > 0
+      Rails.cache.decrement ip_key
+      code = cookies.signed[:code] || rand(1000..9999)
+      code = 1234 if Rails.env.test?
+      Rails.logger.info "#{send_times}: #{I18n.t('sms.user.phone_code', code: code)}"
+      ChinaSMS.to params[:phone], I18n.t('sms.user.phone_code', code: code)
+      cookies.signed[:code] = { value: code, expires: 1.hour.from_now }
+    end
     render nothing: true
   end
 end
