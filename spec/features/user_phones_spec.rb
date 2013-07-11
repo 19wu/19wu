98b859c73d54015fcbb7fcd361ec6ba9fd301003
saberma <mahb45@gmail.com>
2013-07-11 21:16:17 +0800

    test system notfication.

--- spec/features/user_phones_spec.rb
+++ spec/features/user_phones_spec.rb
@@ -2,15 +2,19 @@
 require File.expand_path('../../spec_helper', __FILE__)
 
 feature 'user phones' do
-  given(:user) { create(:user, :confirmed) }
+  given(:user) { create(:user, :confirmed, phone: nil) }
   given(:code) { '1234' }
-  before { sign_in user }
+  before do
+    sign_in user
+    Rails.cache.clear
+  end
   scenario 'I can save phone', js: true do
-    visit edit_user_phone_path
+    click_on I18n.t('flash.user_phones.here')
     fill_in 'phone', with: '13928452888'
     click_on I18n.t('buttons.user_phone.get_code')
     fill_in 'code', with: code
     click_on I18n.t('helpers.submit.user.update')
     page.should have_content(I18n.t('flash.user_phones.updated'))
+    page.should_not have_content(I18n.t('flash.user_phones.here'))
   end
 end
