cc69b4d92d52ddff72096e2d05aff24d3b41fbb9
saberma <mahb45@gmail.com>
2013-07-12 19:46:14 +0800

    upgrade exception_notifier gem to fixed heroku error: undefined method `new' for ExceptionNotifier:Module (NoMethodError)

--- config/initializers/middlewares.rb
+++ config/initializers/middlewares.rb
@@ -1,8 +1,10 @@
 NineteenWu::Application.config.middleware.use :FallbackUrlRedirector
 
 if Rails.env.production?
-  NineteenWu::Application.config.middleware.use ExceptionNotifier,
-    :email_prefix => "[19wu] ",
-    :sender_address => Settings.email.from,
-    :exception_recipients => Settings.email.exception_recipients
+  NineteenWu::Application.config.middleware.use ExceptionNotification::Rack,
+    :email => {
+      :email_prefix => "[19wu] ",
+      :sender_address => Settings.email.from,
+      :exception_recipients => Settings.email.exception_recipients
+    }
 end
