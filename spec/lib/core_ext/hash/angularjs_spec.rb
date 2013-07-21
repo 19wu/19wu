f7dfb9b1a303d36c6a01ebdc0749bfeb10cf0711
saberma <mahb45@gmail.com>
2013-07-21 17:08:06 +0800

    use value.inspect instead of "'#{value}'"

--- spec/lib/core_ext/hash/angularjs_spec.rb
+++ spec/lib/core_ext/hash/angularjs_spec.rb
@@ -2,8 +2,8 @@ require "spec_helper"
 
 describe Hash do
   describe '#to_ng_init' do
-    subject { {a: 1, c:2}.to_ng_init }
-    it { should eql "a=1;c=2" }
+    subject { {a: 1, c:true, d: 's'}.to_ng_init }
+    it { should eql "a=1;c=true;d=\"s\"" }
     context 'with hash value' do
       subject { {a: {b: 1}}.to_ng_init }
       it { should eql "a={\"b\":1}" }
