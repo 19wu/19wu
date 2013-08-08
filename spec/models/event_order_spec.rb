d2429f4959feeba2d33d9df8b8c1734e6d25497b
saberma <mahb45@gmail.com>
2013-08-08 18:44:59 +0800

    decrement event tickets_quantity when order create.

--- spec/models/event_order_spec.rb
+++ spec/models/event_order_spec.rb
@@ -3,29 +3,32 @@ require 'spec_helper'
 describe EventOrder do
   let(:user) { create(:user, :confirmed) }
   let(:event) { create(:event, user: user) }
-  let(:ticket) { create(:event_ticket, event: event, tickets_quantity: 400) }
-  let(:order) do
-    o = event.orders.build user: user, event_id: event.id
-    o.items.build ticket_id: ticket.id, quantity: 1, price: ticket.price
-    o.save
-    o
-  end
+  let(:trade_no) { '2013080841700373' }
+  let(:order) { create(:order_with_items, tickets_quantity: 400, event: event) }
+  subject { order }
+
   describe '#status' do
-    let(:trade_no) { '123' }
-    subject { order }
     its(:pending?) { should be_true }
     its(:paid?) { should be_false }
-    describe '#pay' do
-      before { subject.pay(trade_no) }
-      context 'pending' do
-        its(:pending?) { should be_false }
-        its(:paid?) { should be_true }
-        its(:trade_no) { should eql trade_no }
-      end
-      context 'refund' do # only pending order can be pay
-        before { subject.update_attribute :status, 'refund' }
-        its(:paid?) { should be_false }
-      end
+  end
+
+  describe '#pay' do
+    before { subject.pay(trade_no) }
+    context 'pending' do
+      its(:pending?) { should be_false }
+      its(:paid?) { should be_true }
+      its(:trade_no) { should eql trade_no }
     end
+    context 'refund' do # only pending order can be pay
+      before { subject.update_attribute :status, 'refund' }
+      its(:paid?) { should be_false }
+    end
+  end
+
+  describe 'event tickets quantity' do
+    let(:order) { create(:order_with_items, items_count: 2, event: event) }
+    subject { event }
+    before { order }
+    its(:tickets_quantity) { should eql 398 }
   end
 end
