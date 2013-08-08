85e9654a61a9d11ca21da6bee832fc519c7715b5
saberma <mahb45@gmail.com>
2013-08-08 18:11:09 +0800

    add test for alipay return and notify request.

--- spec/factories/event_orders.rb
+++ spec/factories/event_orders.rb
@@ -5,5 +5,19 @@ FactoryGirl.define do
     event_id 1
     user_id 1
     price 1.5
+
+    factory :order_with_items do
+      ignore do
+        items_count 1
+      end
+      before(:create) do |order, evaluator|
+        FactoryGirl.create_list(:ticket, evaluator.items_count, event: order.event).each do |ticket|
+          order.items.build ticket_id: ticket.id, quantity: 1, price: ticket.price
+        end
+      end
+    end
+  end
+
+  factory :order, parent: :event_order do
   end
 end
--- spec/models/event_order_spec.rb
+++ spec/models/event_order_spec.rb
@@ -1,4 +1,31 @@
 require 'spec_helper'
 
 describe EventOrder do
+  let(:user) { create(:user, :confirmed) }
+  let(:event) { create(:event, user: user) }
+  let(:ticket) { create(:event_ticket, event: event, tickets_quantity: 400) }
+  let(:order) do
+    o = event.orders.build user: user, event_id: event.id
+    o.items.build ticket_id: ticket.id, quantity: 1, price: ticket.price
+    o.save
+    o
+  end
+  describe '#status' do
+    let(:trade_no) { '123' }
+    subject { order }
+    its(:pending?) { should be_true }
+    its(:paid?) { should be_false }
+    describe '#pay' do
+      before { subject.pay(trade_no) }
+      context 'pending' do
+        its(:pending?) { should be_false }
+        its(:paid?) { should be_true }
+        its(:trade_no) { should eql trade_no }
+      end
+      context 'refund' do # only pending order can be pay
+        before { subject.update_attribute :status, 'refund' }
+        its(:paid?) { should be_false }
+      end
+    end
+  end
 end
