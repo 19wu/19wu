4dc9e27a8af1848a6b93fc392be76ac638768dad
saberma <mahb45@gmail.com>
2013-08-11 10:39:41 +0800

    to_ng_init should support nil value.

--- lib/core_ext/hash/angularjs.rb
+++ lib/core_ext/hash/angularjs.rb
@@ -2,12 +2,10 @@ class Hash
   # {a:1, b:2}.to_ng_init # a=1;b=2
   def to_ng_init
     self.inject([]) do |result,(key,value)|
-      value = if value.is_a?(Hash)
-                value.to_json
-              elsif value.is_a?(Array)
+      value = if value.is_a?(Array)
                 "[#{value.map(&:to_json).join(',')}]"
               else
-                value.inspect
+                value.to_json
               end
       result << "#{key}=#{value}"
     end.join(';')
