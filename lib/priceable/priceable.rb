a022a63868276c97f4454e7c56719d91b768e906
jasl <jasl9187@hotmail.com>
2013-08-19 03:10:20 +0800

    fix hack bug

--- lib/priceable/priceable.rb
+++ lib/priceable/priceable.rb
@@ -1,15 +1,17 @@
 # https://github.com/willcosgrove/priceable, hack this cause it is conflict with observer
 module Priceable
-  SUFFIXES = ["_in_cents", "_in_pennies", "_as_integer"]
   def priceable(*price_fields)
     price_fields.each do |price_field|
+      suffix = '_in_cents'
+
       define_method price_field do
-        unless send("#{price_field}#{suffix}".to_sym).nil?
+        if send("#{price_field}#{suffix}".to_sym)
           send("#{price_field}#{suffix}".to_sym) / 100.0
         else
           0.0
         end
       end
+
       define_method "#{price_field}=".to_sym do |new_price|
         send("#{price_field}#{suffix}=".to_sym, (new_price.to_f * 100).round)
       end
@@ -20,7 +22,6 @@ module Priceable
         attr_accessible *price_fields
       end
     end
-
   end
 end
 
