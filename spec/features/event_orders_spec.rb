67a7e93337a6ba3927825c86fbc4a7f16390f803
saberma <mahb45@gmail.com>
2013-08-28 19:53:50 +0800

    add feature test.

--- spec/features/event_orders_spec.rb
+++ spec/features/event_orders_spec.rb
@@ -8,63 +8,105 @@ feature 'event orders', js: true do
   given(:address_attributes) { attributes_for(:shipping_address) }
 
   before do
-    sign_in user
     event.update_attribute :tickets_quantity, 20
     Timecop.travel(Date.parse('2013-08-25'))
   end
 
-  context 'with user name and phone' do
-    before { user.profile.update_attribute :name, 'saberma' }
-    scenario 'I can create order' do
-      ticket.update_attribute :price, 299
-      visit event_path(event)
-      within '.event-tickets' do
-        select '1'
+  context 'login user' do
+    before { sign_in user }
+    context 'with user name and phone' do
+      before { user.profile.update_attribute :name, 'saberma' }
+      scenario 'I can create order' do
+        ticket.update_attribute :price, 299
+        visit event_path(event)
+        within '.event-tickets' do
+          select '1'
+        end
+        find('a', text: '购买').click
+        expect(page).to have_content('您已经提交了订单，订单号为 201308250001，请尽快支付')
+        expect(page).to have_content('使用支付宝支付')
       end
-      find('a', text: '购买').click
-      expect(page).to have_content('您已经提交了订单，订单号为 201308250001，请尽快支付')
-      expect(page).to have_content('使用支付宝支付')
+
+      scenario 'I buy a ticket with invoice' do
+        ticket.update_attributes require_invoice: true, price: 299
+        visit event_path(event)
+        within '.event-tickets' do
+          select '1'
+        end
+        find('a', text: '购买').click
+        fill_in 'invoice_title', with: address_attributes[:invoice_title]
+        select ChinaCity.get(address_attributes[:province])
+        select ChinaCity.get(address_attributes[:city])
+        select ChinaCity.get(address_attributes[:district])
+        fill_in 'address', with: address_attributes[:address]
+        fill_in 'shipping_name', with: address_attributes[:name]
+        fill_in 'shipping_phone', with: address_attributes[:phone]
+        find('a', text: '购买').click
+        expect(page).to have_content('使用支付宝支付')
+      end
+
+      scenario 'I buy a free ticket' do
+        visit event_path(event)
+        within '.event-tickets' do
+          select '1'
+        end
+        find('a', text: '购买').click
+        expect(page).to have_content('您已经提交了订单，订单号为 201308250001，此订单为免费订单，不需要支付，谢谢。')
+      end
+
+      scenario 'I want to buy tickets, but sold out' do
+        event.update_attribute :tickets_quantity, 0
+        visit event_path(event)
+        expect(page).to have_selector('.sold-out')
+      end
     end
 
-    scenario 'I buy a ticket with invoice' do
-      ticket.update_attributes require_invoice: true, price: 299
-      visit event_path(event)
-      within '.event-tickets' do
-        select '1'
+    context 'without user name and phone' do
+      scenario 'I can create order' do
+        visit event_path(event)
+        within '.event-tickets' do
+          select '1'
+        end
+        find('a', text: '购买').click
+        within '.event-tickets' do
+          fill_in 'user_name', with: '张三'
+          fill_in 'user_phone', with: '13928452888'
+        end
+        find('a', text: '购买').click
+        expect(page).to have_content('您已经提交了订单，订单号为 201308250001，此订单为免费订单，不需要支付，谢谢。')
       end
-      find('a', text: '购买').click
-      fill_in 'invoice_title', with: address_attributes[:invoice_title]
-      select ChinaCity.get(address_attributes[:province])
-      select ChinaCity.get(address_attributes[:city])
-      select ChinaCity.get(address_attributes[:district])
-      fill_in 'address', with: address_attributes[:address]
-      fill_in 'shipping_name', with: address_attributes[:name]
-      fill_in 'shipping_phone', with: address_attributes[:phone]
-      find('a', text: '购买').click
-      expect(page).to have_content('使用支付宝支付')
     end
+  end
 
-    scenario 'I buy a free ticket' do
+  context 'as guest user' do
+    scenario 'I can register and order in place' do
       visit event_path(event)
+      within('.event-tickets') { select '1' }
+      find('a', text: '购买').click
+      within '.signup' do
+        fill_in 'user_login', with: 'someone'
+        fill_in 'user_email', with: 'someone@example.com'
+        fill_in 'user_password', with: '666666'
+        find('a', text: '注册').click
+      end
       within '.event-tickets' do
-        select '1'
+        fill_in 'user_name', with: '张三'
+        fill_in 'user_phone', with: '13928452888'
       end
       find('a', text: '购买').click
       expect(page).to have_content('您已经提交了订单，订单号为 201308250001，此订单为免费订单，不需要支付，谢谢。')
     end
-
-    scenario 'I want to buy tickets, but sold out' do
-      event.update_attribute :tickets_quantity, 0
+    scenario 'I can login and order in place' do
       visit event_path(event)
-      expect(page).to have_selector('.sold-out')
-    end
-  end
-
-  context 'without user name and phone' do
-    scenario 'I can create order' do
-      visit event_path(event)
-      within '.event-tickets' do
-        select '1'
+      within('.event-tickets') { select '1' }
+      find('a', text: '购买').click
+      within '.signup' do
+        find('a', text: '登录').click
+      end
+      within '.login' do
+        fill_in 'email', with: user.email
+        fill_in 'password', with: user.password
+        find('a', text: '登录').click
       end
       find('a', text: '购买').click
       within '.event-tickets' do
