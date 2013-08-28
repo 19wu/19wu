d2de66452dacdfa830dc6d5353ff2fbfcd36f8b3
saberma <mahb45@gmail.com>
2013-08-29 00:09:52 +0800

    fixed alipay notify fail.

--- app/controllers/user_orders_controller.rb
+++ app/controllers/user_orders_controller.rb
@@ -1,6 +1,7 @@
 class UserOrdersController < ApplicationController
   include AlipayGeneratable
   before_filter :authenticate_user!
+  skip_before_filter  :verify_authenticity_token, :alipay_notify
 
   def index
     @orders = current_user.orders.includes(items: :ticket)
