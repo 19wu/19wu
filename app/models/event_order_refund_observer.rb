73bcb57a7b3590b89d72b2990c0807ea2454001d
saberma <mahb45@gmail.com>
2013-09-18 08:51:47 +0800

    add refund observer test.

--- app/models/event_order_refund_observer.rb
+++ app/models/event_order_refund_observer.rb
@@ -2,4 +2,8 @@ class EventOrderRefundObserver< ActiveRecord::Observer
   def after_refund(refund, transition)
     refund.order.update_attribute :refunded_amount, refund.amount
   end
+
+  def after_submit(refund, transition)
+    OrderMailer.delay.notify_support_refund(refund)
+  end
 end
--- spec/models/event_order_refund_observer_spec.rb
+++ spec/models/event_order_refund_observer_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 
 describe EventOrderRefundObserver do
+  let(:user) { create(:user, :confirmed) }
+  let(:event) { create(:event, user: user) }
+  let(:order) { create(:order_with_items, event: event) }
+  let(:trade_no) { '2013080841700373' }
+  let(:refund) { order.refunds.create amount: '10', reason: 'test' }
+
+  before { order.pay(trade_no) }
+
+  context 'submit' do
+    let(:mail) { double('mail') }
+    it 'should notify all followers' do
+      mail.should_receive(:deliver)
+      OrderMailer.should_receive(:notify_support_refund).and_return(mail)
+      refund.submit!
+    end
+  end
 end
