1c2e68af64fc0a09a7602ef243a1a0f02b49f320
saberma <mahb45@gmail.com>
2013-09-19 15:27:40 +0800

    show global amount and reason for convenient.

--- spec/controllers/order_refunds_controller_spec.rb
+++ spec/controllers/order_refunds_controller_spec.rb
@@ -5,18 +5,27 @@ describe OrderRefundsController do
   let(:event) { create(:event, user: user) }
   let(:order) { create(:order_with_items, event: event) }
   let(:trade_no) { '2013080841700373' }
-  let(:refund) { order.refunds.submit amount: '10', reason: 'test' }
-  let(:refund_batch) { RefundBatch.create }
-  let(:result_details) { "2013080841700373^10^SUCCESS$jax_chuanhang@alipay.com^2088101003147483^0.01^SUCCESS" }
 
-  before do
-    order.pay(trade_no)
-    refund.update_attribute :refund_batch_id, refund_batch.id
+  before { order.pay(trade_no) }
+
+  describe "POST submit" do
+    before { login_user user }
+    let(:refund) { build :event_order_refund, order: order }
+    it "should be success" do
+      post :submit, event_id: event.id, id: order.id, refund: { amount: refund.amount, reason: refund.reason }, format: 'json'
+      expect(response).to be_success
+    end
   end
 
   describe "POST alipay_notify" do
+    let(:refund) { create :event_order_refund, :submited, order: order }
+    let(:refund_batch) { RefundBatch.create }
+    let(:result_details) { "2013080841700373^10^SUCCESS$jax_chuanhang@alipay.com^2088101003147483^0.01^SUCCESS" }
     let(:attrs) { { batch_no: refund_batch.batch_no, notify_id: '123', result_details: result_details, success_num: '1' } }
-    before { Alipay::Notify.stub(:verify?).and_return(true) }
+    before do
+      Alipay::Notify.stub(:verify?).and_return(true)
+      refund.update_attribute :refund_batch_id, refund_batch.id
+    end
     it "should be success" do
       post :alipay_notify, attrs.merge(sign_type: 'md5', sign: Alipay::Sign.generate(attrs))
       expect(response).to be_success
