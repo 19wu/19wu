f8ef2a1393f9472fcfe206d27b8c6d0cf7221b4c
saberma <mahb45@gmail.com>
2015-04-25 10:44:52 +0800

    use qiniu cdn.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ NineteenWu::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "//dn-nineteen-wu.qbox.me"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( locales/zh-CN.js )
