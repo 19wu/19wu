26aea75e9058b235e2b26c4109fc81a965c8594f
saberma <mahb45@gmail.com>
2015-05-17 21:44:45 +0800

    add require_invoice attribute to orders.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150501061606) do
+ActiveRecord::Schema.define(version: 20150516142649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,8 +35,8 @@ ActiveRecord::Schema.define(version: 20150501061606) do
   create_table "event_changes", force: true do |t|
     t.integer  "event_id"
     t.string   "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "event_changes", ["event_id"], name: "index_event_changes_on_event_id", using: :btree
@@ -104,16 +104,17 @@ ActiveRecord::Schema.define(version: 20150501061606) do
   add_index "event_order_status_transitions", ["event_order_id"], name: "index_event_order_status_transitions_on_event_order_id", using: :btree
 
   create_table "event_orders", force: true do |t|
-    t.integer  "event_id",                                    null: false
-    t.integer  "user_id",                                     null: false
-    t.integer  "quantity",                                    null: false
+    t.integer  "event_id",                                        null: false
+    t.integer  "user_id",                                         null: false
+    t.integer  "quantity",                                        null: false
     t.string   "status",               limit: 16
     t.string   "trade_no",             limit: 32
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "price_in_cents",                  default: 0, null: false
-    t.string   "number",               limit: 16,             null: false
-    t.integer  "paid_amount_in_cents",            default: 0, null: false
+    t.integer  "price_in_cents",                  default: 0,     null: false
+    t.string   "number",               limit: 16,                 null: false
+    t.integer  "paid_amount_in_cents",            default: 0,     null: false
+    t.boolean  "require_invoice",                 default: false
   end
 
   add_index "event_orders", ["event_id"], name: "index_event_orders_on_event_id", using: :btree
@@ -143,8 +144,8 @@ ActiveRecord::Schema.define(version: 20150501061606) do
     t.datetime "end_time"
     t.string   "location"
     t.text     "content"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "user_id",                      null: false
     t.text     "location_guide"
     t.integer  "group_id",                     null: false
@@ -156,8 +157,8 @@ ActiveRecord::Schema.define(version: 20150501061606) do
   create_table "fallback_urls", force: true do |t|
     t.string   "origin"
     t.string   "change_to"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "fallback_urls", ["origin"], name: "index_fallback_urls_on_origin", unique: true, using: :btree
@@ -168,8 +169,8 @@ ActiveRecord::Schema.define(version: 20150501061606) do
     t.integer  "follower_id",                     null: false
     t.string   "follower_type",                   null: false
     t.boolean  "blocked",         default: false, null: false
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "follows", ["followable_id", "followable_type"], name: "fk_followables", using: :btree
@@ -178,16 +179,16 @@ ActiveRecord::Schema.define(version: 20150501061606) do
   create_table "group_collaborators", force: true do |t|
     t.integer  "group_id"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "group_topic_replies", force: true do |t|
     t.text     "body"
     t.integer  "group_topic_id", null: false
     t.integer  "user_id",        null: false
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "group_topic_replies", ["group_topic_id"], name: "index_group_topic_replies_on_group_topic_id", using: :btree
@@ -197,8 +198,8 @@ ActiveRecord::Schema.define(version: 20150501061606) do
     t.text     "body"
     t.integer  "user_id",                   null: false
     t.integer  "group_id",                  null: false
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "replies_count", default: 0
   end
 
@@ -207,8 +208,8 @@ ActiveRecord::Schema.define(version: 20150501061606) do
   create_table "groups", force: true do |t|
     t.integer  "user_id",    null: false
     t.string   "slug",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "groups", ["slug"], name: "index_groups_on_slug", unique: true, using: :btree
@@ -216,8 +217,8 @@ ActiveRecord::Schema.define(version: 20150501061606) do
   create_table "photos", force: true do |t|
     t.integer  "user_id",    null: false
     t.string   "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "profiles", force: true do |t|
@@ -225,8 +226,8 @@ ActiveRecord::Schema.define(version: 20150501061606) do
     t.string   "website"
     t.text     "bio"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
@@ -258,8 +259,8 @@ ActiveRecord::Schema.define(version: 20150501061606) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                        null: false
-    t.datetime "updated_at",                                        null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "login"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
