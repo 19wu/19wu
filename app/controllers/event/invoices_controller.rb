4a9581d074f179958db7ab55f11c1f999b9e62ce
saberma <mahb45@gmail.com>
2015-05-17 22:03:45 +0800

    update invoice export sql.

--- app/controllers/event/invoices_controller.rb
+++ app/controllers/event/invoices_controller.rb
@@ -3,6 +3,6 @@ class Event::InvoicesController < ApplicationController
   before_filter :authorize_event!
 
   def index
-    @orders = @event.orders.paid.joins(items: :ticket).where(event_tickets: {require_invoice: true}).order('paid_amount_in_cents asc, id asc')
+    @orders = @event.orders.paid.where(require_invoice: true).includes(:items).order('paid_amount_in_cents asc, id asc')
   end
 end
